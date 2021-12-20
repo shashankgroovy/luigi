394592ef58b732c21539906ef0542aed1d99ee83
Shashank Srivastav <shashankgrovy@gmail.com>
2021-12-20 18:41:04 +0530

    Updates to README and minor schema changes

--- api/v1/model/search.py
+++ api/v1/model/search.py
@@ -12,7 +12,7 @@ def save_search(search_term, external_ip):
     # Obtain a connection to the database
 
     try:
-        query = "INSERT INTO searches (externalUserIp, query) VALUES (?, ?)"
+        query = "INSERT INTO searches (client_ip, search) VALUES (?, ?)"
         g.db.execute(query, (external_ip, search_term))
         g.db.commit()
 
